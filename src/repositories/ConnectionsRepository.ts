d7242d6942e70a62309394b99a89c02adc5a6318
Raissa B <raissabatista3190@gmail.com>
2021-04-25 00:33:20 -0300

    first commit

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,12 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+
+import { Connection } from "../entities/Connection";
+
+
+@EntityRepository(Connection)
+class ConnectionsRepository extends Repository<Connection>{
+
+
+}
+
+export { ConnectionsRepository};
